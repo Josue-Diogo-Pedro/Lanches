297c980b7afdcff233d0a245cd58bf5b09059721
Josue-Diogo-Pedro <pedrojosuediogo@gmail.com>
2022-10-26 14:03:24 +0100

    first commit

---
+++ Lanches/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lanches.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
