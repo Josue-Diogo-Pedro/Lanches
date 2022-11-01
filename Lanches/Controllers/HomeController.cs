aea8bd0b328539301703d5d99fc0672820ef11e7
Josue-Diogo-Pedro <pedrojosuediogo@gmail.com>
2022-11-01 08:49:09 +0100

    Inserindo carousel

--- Lanches/Controllers/HomeController.cs
+++ Lanches/Controllers/HomeController.cs
@@ -6,19 +6,9 @@ namespace Lanches.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+       
 
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
